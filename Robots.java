fe27c6146c85b94dae9f5e452952aa802a04cbfe
KvasarINC <99033916+KvasarINC@users.noreply.github.com>
2022-02-19 17:12:07 +0200

    Create Robots.java

---
+++ Robots.java
@@ -0,0 +1,28 @@
+package com.learning;
+
+import java.util.Scanner;
+
+public class Robots {
+
+    private String name;
+    private int hight;
+    private boolean warm;
+
+    public void setName(String x) {
+        name = x;
+    }
+
+    public void setHight(int x) {
+        hight = x;
+    }
+
+    public void setWarm(boolean x) {
+        warm = x;
+    }
+
+    public void PrintParament() {
+        System.out.println(name);
+        System.out.println(hight);
+    }
+}
+
